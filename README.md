6499347376d767e2a51dc514b1e9f7f8885aa829
ihsansunman <ihsansunman@yandex.com>
2022-04-25 10:57:01 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+![License](https://img.shields.io/github/license/ihsansunman/ibb-metro-map?style=for-the-badge) ![issues](https://img.shields.io/github/issues/ihsansunman/ibb-metro-map?style=for-the-badge) ![stars](https://img.shields.io/github/stars/ihsansunman/ibb-metro-map?style=for-the-badge) ![REACTJS](https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=for-the-badge) ![MUI](https://img.shields.io/badge/-mui-007FFF?logo=mui&logoColor=white&style=for-the-badge) ![Netlify](https://img.shields.io/badge/-netlify-00C7B7?logo=netlify&logoColor=white&style=for-the-badge)
+
+
+
+# IBB Metro Map
+
+## Screenshots
+
+![Screenshot](https://via.placeholder.com/468x300?text=Coming+soon)
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/ihsansunman/ibb-metro-map
+```
+
+Go to the project directory
+
+```bash
+  cd ibb-metro-map
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+## Packages
+
+- MUI
+- IBB Metro MAP API
+
+## Authors
+
+- [@ihsansunman](https://www.github.com/ihsansunman)
+
+
+## Feedback
+
+If you have any feedback, please reach out to us at [ihsansunman@asnus.com](mailto:ihsansunman@asnus.com)
---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './style.css';
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const axios = require('axios');
+  const URL = 'https://api.ibb.gov.tr/MetroIstanbul/api/MetroMobile/V2/GetMaps';
+
+  axios.get(URL).then(function (response) {
+    setData(response.data.Data);
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  if (loading) {
+    return (
+      <Stack spacing={1}>
+        <Skeleton variant="text" height={100} />
+        <Skeleton variant="rectangular" height={650} />
+      </Stack>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Metro İstanbul Ağ Haritası Listesi</h1>
+
+      {data.map((data) => (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>{data.Title}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <a href={data.ImageURL} target="_blank">
+                <img src={data.IconURL} />
+              </a>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
+  );
+}
